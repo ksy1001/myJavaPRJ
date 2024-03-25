7eb48e0e67af41333f0db9540873b504f266549d
ksy1001 <kj0976@naver.com>
2024-03-25 14:22:43 +0900

    20240325자바수업4

--- src/main/java/Code04_13.java
+++ src/main/java/Code04_13.java
@@ -4,14 +4,18 @@ public class Code04_13 {
         String cuStr = "";
         String upper = "";
         String lower = "";
+        String allStr = "";
 
         cuStr = str.trim();
         upper = str.toUpperCase();
         lower = str.toLowerCase();
+        allStr = str.replaceAll(" ", "");
+
 
         System.out.println("기존 문자열 ==> [" + str + "]");
         System.out.println("공백 제거 ==> [" + cuStr + "]");
         System.out.println("대문자 ==> [" + upper + "]");
         System.out.println("소문자 ==> [" + lower + "]");
+        System.out.println("replace_all ==> [" + allStr + "]");
     }
 }
