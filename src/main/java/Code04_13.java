bb7bf374f84ce9adea50a8f798a77c42f85b942f
ksy1001 <kj0976@naver.com>
2024-03-25 14:14:26 +0900

    20240325자바수업4

--- src/main/java/Code04_13.java
+++ src/main/java/Code04_13.java
@@ -2,10 +2,16 @@ public class Code04_13 {
     public static void main(String[]args){
         String str = " 한글  ABCD efgh  ";
         String cuStr = "";
+        String upper = "";
+        String lower = "";
 
         cuStr = str.trim();
+        upper = str.toUpperCase();
+        lower = str.toLowerCase();
 
         System.out.println("기존 문자열 ==> [" + str + "]");
         System.out.println("공백 제거 ==> [" + cuStr + "]");
+        System.out.println("대문자 ==> [" + upper + "]");
+        System.out.println("소문자 ==> [" + lower + "]");
     }
 }
