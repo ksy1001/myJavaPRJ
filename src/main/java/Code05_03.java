fa45b8bde45c9f5a1fcf187081835c3db93b5521
ksy1001 <kj0976@naver.com>
2024-04-01 14:12:39 +0900

    2024/04/01

---
+++ src/main/java/Code05_03.java
@@ -0,0 +1,15 @@
+public class Code05_03 {
+    public static void main(String[]args){
+        int num = 200;
+
+        if (num < 100) {
+            System.out.println("100보다 작군요.");
+            System.out.println("여기는 팩트다..");
+        } else {
+            System.out.println("100보다 크군요.");
+            System.out.println("여기는 틀렸다..");
+        }
+
+        System.out.println("프로그램 끝");
+    }
+}
