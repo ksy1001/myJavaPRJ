9a73f26fe70ccb6cf0e9bd761329b83e0b98b1d5
ksy1001 <kj0976@naver.com>
2024-04-09 13:29:25 +0900

    2024/04/0

---
+++ src/main/java/SelfStudy/SelfStudy0409.java
@@ -0,0 +1,27 @@
+package SelfStudy;
+
+import java.util.Scanner;
+
+public class SelfStudy0409 {
+public static void main(String[]args){
+    Scanner s = new Scanner(System.in);
+    String myhand, comhand;
+    System.out.print("나의 가위/바위/보 ==> ");
+    myhand = s.next();
+
+    String[] hands = {"가위", "바위", "보"};
+    int rnd = (int) (Math.random() * hands.length);
+    comhand = hands[rnd];
+    System.out.println("컴퓨터의 가위/바위/보 ==> "+ comhand);
+
+    if (myhand.equals("가위")){
+        if (comhand.equals("가위"))
+            System.out.println("무승부!");
+        else if (comhand.equals("바위"))
+            System.out.println("패배!");
+        else if (comhand.equals("보"))
+        System.out.println("승리!");
+    }
+
+    }
+}
