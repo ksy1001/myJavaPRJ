b6b525910bb51581fbe2e1c1f7fabfb52febd52a
ksy1001 <kj0976@naver.com>
2024-04-15 13:23:42 +0900

    2024/04/15 커밋 안해놨던 코드

---
+++ src/main/java/Code6_6.java
@@ -0,0 +1,9 @@
+public class Code6_6 {
+    public static void main(String [] args){
+        int hap=0;
+        for (int i=1; i <=10; i++){
+            hap= hap + i;
+        }
+        System.out.println("1부터 10까지의 합계:"+hap);
+    }
+}
