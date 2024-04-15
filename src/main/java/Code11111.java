e59f09a431cc3b0e8ca94761e55773f0edf1871a
ksy1001 <kj0976@naver.com>
2024-04-15 13:23:14 +0900

    2024/04/15 자바 쪽지시험

---
+++ src/main/java/Code11111.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Code11111 {
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        int a, b, hap;
+        hap = 0;
+        a = s.nextInt();
+        b = s.nextInt();
+
+        if (a < b) {
+            for (int i = a; i <=b; i++){
+                if (i % 2 !=0){
+                    hap += i;
+                }
+            }
+        }else if (a >b) {
+            for (int i = a; i >=b; i--){
+                if (i % 2 !=0){
+                    hap += i;
+                }
+            }
+        }else {
+            System.out.println("a와 b가 같습니다. 다시 입력해주세요");
+        }
+        System.out.println(hap);
+        s.close();
+    }
+}
