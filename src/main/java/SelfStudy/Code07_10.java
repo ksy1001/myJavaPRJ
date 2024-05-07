d4d0d6a80d4231de6a40a732a51985bccd943198
ksy1001 <kj0976@naver.com>
2024-05-07 14:45:23 +0900

    토끼쓰

---
+++ src/main/java/SelfStudy/Code07_10.java
@@ -0,0 +1,23 @@
+package SelfStudy;
+class Rabbit {
+    String shape;
+    int xPos;
+    int yPos;
+
+    Rabbit() {
+        shape = "토끼";
+    }
+
+    void setPosition(int x, int y){
+        xPos = x;
+        yPos = y;
+    }
+}
+
+public class Code07_10 {
+    public static void main(String[]args){
+        Rabbit rabbit = new Rabbit();
+
+        System.out.printf("토끼 객체의 기본 모양은 [%s]입니다.", rabbit.shape);
+    }
+}
