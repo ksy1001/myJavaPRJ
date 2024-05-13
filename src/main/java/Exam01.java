169e29abf8040baa7f848aa0354364495f7b460d
ksy1001 <kj0976@naver.com>
2024-05-13 13:14:26 +0900

    차쉑

--- src/main/java/Exam01.java
+++ src/main/java/Exam01.java
@@ -1,24 +1,19 @@
-import java.util.Scanner;
+class Car{
+    private int speed;
+    private  String color;
 
-public class Exam01 {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-    int a, b;
-    System.out.print("첫 번째 : ");
-    a = s.nextInt();
-
-    System.out.print("두 번째 : ");
-    b = s.nextInt();
-
-    if (a > b) {
-        System.out.println("a는 b보다 큽니다.");
-
-    } else if (a == b) {
-        System.out.println("a는 b와 같습니다.");
-
-    } else {
-        System.out.println("a는 b보다 작습니다.");
+    void setCar(int speed, String color){
+        this.speed = speed;
+        this.color = color;
     }
-    s.close();
+
+    Car(int speed){
+        this.speed = speed;
+    }
+}
+
+public class Exam01{
+    public static void main(String[] args) {
+        Car car = new Car(100);
     }
 }
--- src/main/java/SelfStudy/Exam01.java
+++ src/main/java/SelfStudy/Exam01.java
@@ -1,15 +1,19 @@
-package SelfStudy; //패키지명
-public class Exam01 { //클래스명
-    public static void main(String[]args){ //메인함수 메서드
+class Car{
+    private int speed;
+    private  String color;
 
-        for (int i = 0;  i<4;  i++) { // 행의 수를 정하는 for 문. i를 0으로 시작한다. i가 3이 될때까지, i는 1씩 증가한다. 총4행.
-            for(int blank = 1;  blank<4-i;  blank++) { // i값에 따라 공백의 수를 정하는 for 문. 첫 공백은 3개로 시작해서, 행마다 1개씩 줄어든다.
-                System.out.print(" "); // 위의 for 문에 따라, 공백을 출력
+    void setCar(int speed, String color){
+        this.speed = speed;
+        this.color = color;
+    }
 
-            }for (int star = 0;  star<i*2+1;  star++) { // 각 행마다 출력되는 별표의 수를 정하는 for 문
-                // 초기값을 0으로 시작해서, i값(몇행인지)에 따라 출력되는 별의 수가 바뀐다. 각 행마다 별의 수는 1,3,5,7개가 출력된다.
-                System.out.print("*"); //for 문에 따라서 * 표시를 출력한다.
-            }System.out.println(); // 줄바꿈.
-        }
+    Car(int speed){
+        this.speed = speed;
+    }
+}
+
+public class Exam01{
+    public static void main(String[] args) {
+        Car car = new Car(100);
     }
 }
